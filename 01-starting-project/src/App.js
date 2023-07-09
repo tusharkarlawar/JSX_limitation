9fa971d661caef1da918e8155487f5e7d5cbc97c
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-07-09 15:38:51 +0530

    refs

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -6,11 +6,11 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, uCollegename) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge, collegename:uCollegename, id: Math.random().toString() },
       ];
     });
   };
